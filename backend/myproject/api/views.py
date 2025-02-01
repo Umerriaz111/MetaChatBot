50516ddf6d841ae8c713da193e624c7dc2665f11
Umerriaz111 <umerriaz5646@gmail.com>
2025-02-01 19:54:37 +0500

    Working Prototype

--- backend/myproject/api/views.py
+++ backend/myproject/api/views.py
@@ -60,39 +60,11 @@ class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-# Create your views here.
-# @api_view(['GET'])
-# def search(request):
-#     if request.method == 'GET':
-#         try:
-#             search = SearxSearchWrapper(searx_host="http://127.0.0.1:8080")
-#             query = request.GET.get('query')
-#             response = search.run(query)
-#             return Response(response)
-#         except Exception as e:
-#             return Response(f'Error due to {e}',status=status.HTTP_400_BAD_REQUEST)
-# @api_view(['GET'])
-# def search(request):
-#     if request.method == 'GET':
-#         try:
-#             query = request.GET.get('query')
-#             if not query:
-#                 return Response('Query parameter is missing', status=status.HTTP_400_BAD_REQUEST)
-
-#             # Log the query for debugging
-#             print(f"Received query: {query}")
-            
-
-#             search = SearxSearchWrapper(searx_host="http://127.0.0.1:8080")
-#             response = search.run(query)
-#             output={"response":response}
-#             print( "This is response", output)
-#             return Response(output)
-#         except Exception as e:
-#             print(f"Error: {str(e)}")  # Log the error for debugging
-#             return Response(f'Error due to {e}', status=status.HTTP_400_BAD_REQUEST)
-        
-        
+## Azam this Method is Working This .Write like this in Langchain . 
+## {
+#     "detail": "Authentication credentials were not provided."
+# }
+# Remove Authentication credentials so far We Can Add that after Testing is Done
 @api_view(['GET'])
 def search(request):
     if request.method == 'GET':
